81be53bcb0af619c085ced925bc25fe1f5aabf81
HarshadSangale <harshadindia1@gmail.com>
2023-09-07 01:57:32 -0400

    Intital Commit

---
+++ static/index.js
@@ -0,0 +1,7 @@
+var audio = new Audio("../static/Audios/hd-by-gvh-sounds.mp3")
+// var img = document.getElementsByClassName("f");
+
+function play() {
+    audio.play();
+    console.log("play");
+}
