d09fdf2b8e66483073a1dfa8addea501d7df357c
harshadtypes <harshadindia1@gmail.com>
2023-09-20 19:05:15 -0400

    main commits

---
+++ static/index.js
@@ -0,0 +1,7 @@
+var audio = new Audio("../static/Audios/hd-by-gvh-sounds.mp3")
+var log = document.getElementsByClassName('logo');
+
+function play() {
+    audio.play();
+    console.log("play");
+}
