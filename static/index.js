1b210e940df8114cbf9a2bda0cf997e638585428
harshadtypes <harshadindia1@gmail.com>
2023-10-18 22:25:42 -0400

    pest control

--- static/index.js
+++ static/index.js
@@ -1,7 +1,9 @@
 var audio = new Audio("../static/Audios/hd-by-gvh-sounds.mp3")
 var log = document.getElementsByClassName('logo');
+var imag = document.getElementById('compil');
 
 function play() {
     audio.play();
+    imag.src = "/static/UI_Elements/flavours/flav.jpg";
     console.log("play");
 }
