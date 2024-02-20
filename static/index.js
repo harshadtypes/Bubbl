63b51f976ead44f4e054efe6fb3f6bcfc0f27b17
harshadtypes <harshadindia1@gmail.com>
2024-02-19 21:16:26 -0500

    initial-commit

--- static/index.js
+++ static/index.js
@@ -1,10 +1,10 @@
-var audio = new Audio("../static/Audios/hd-by-gvh-sounds.mp3")
-var log = document.getElementsByClassName('logo');
-var imag = document.getElementById('compil');
+// var audio = new Audio("../static/Audios/hd-by-gvh-sounds.mp3")
+// var log = document.getElementsByClassName('logo');
+// var imag = document.getElementById('compil');
 // var text = document.getElementById('white-desc');
 
 function play() {
-    audio.play();
+    // audio.play();
     imag.src = "/static/UI_Elements/flavours/flav.jpg";
     // text.style.display = "block";  
     console.log("play");
